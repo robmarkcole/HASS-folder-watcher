b67dcd2591f532b4f4544bd7f7966822d10beac5
robmarkcole <robmarkcole@gmail.com>
2018-03-04 21:33:34 +0000

    Simplify

--- custom_components/sensor/folder_watcher.py
+++ custom_components/sensor/folder_watcher.py
@@ -1,12 +1,8 @@
 """
-Sensor for monitoring activity on a folder.
+Component for monitoring activity on a folder.
 """
-#import datetime
-#from datetime import timedelta
 import logging
-import os
 import voluptuous as vol
-import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
@@ -14,6 +10,7 @@ from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 
+#DEPENDENCIES = ['watchdog']
 _LOGGER = logging.getLogger(__name__)
 
 CONF_PATH = 'folder'
@@ -29,99 +26,43 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if not hass.config.is_allowed_path(path):
         _LOGGER.error("folder %s is not valid or allowed", path)
     else:
-        folder_watcher = Watcher(path)
-        add_devices([folder_watcher], True)
+        watcher = Watcher(path)
+        add_devices([watcher], True)
 
 
 class Watcher(Entity):
-    """Class for watching a folder, state recorded in a dict."""
-
-    ICON = 'mdi:folder'
+    """Class for watching a filesystem."""
 
     def __init__(self, path):
-        self._path = os.path.join(path, '')  # Ass trailing /
-        event = WatchdogEvent()  # Init the data object
-        self._event = event
         self._observer = Observer()
-        self._observer.schedule(MyHandler(event), self._path, recursive=True)
+        self._observer.schedule(
+            MyHandler(self.fire_event), path, recursive=True)
         self._observer.start()
 
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return "folder_watcher"
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        data = self._event.data
-        event_str = data["event"] + " : " + data["file"]
-        return event_str
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self.ICON
-
-    @property
-    def device_state_attributes(self):
-        """Return other details about the sensor state."""
-        attr = {
-            'path': self._path
-            }
-        return attr
-
-
-class WatchdogEvent():
-    """Class representing the last event."""
-    def __init__(self):
-        data = {
-            "time": "no_data",
-            "event": "no_data",
-            "full_path": "no_data",
-            "file": "no_data"
-            }
-        self._data = data
-
-    def update(self, data):
-        """Update with recent data."""
-        self._data = data
-
-    @property
-    def data(self):
-        """Return the last event."""
-        return self._data
+    def fire_event(self, data):
+        _LOGGER.warning("WATCHDOG {} {}".format(
+            data["event_type"], data["src_path"]))
 
 
 class MyHandler(PatternMatchingEventHandler):
     patterns = ["*.txt", "*.py", "*.md", "*.jpg", "*.png"]
 
-    def __init__(self, event_data):
+    def __init__(self, fire_event):
         super().__init__()
-        self._event_data = event_data
+        self.fire_event = fire_event
 
     def process(self, event):
-        """
-        event.event_type
-            'modified' | 'created' | 'moved' | 'deleted'
-        event.is_directory
-            True | False
-        event.src_path
-            path/to/observed/file
-        """
-        # the file will be processed there
+        """Process the Watchdog event."""
         data = {
-            "time": time.strftime("%Y-%m-%d %H:%M"),
-            "event": event.event_type,
-            "full_path": event.src_path,
-            "file": os.path.split(event.src_path)[-1]
+            "event_type": event.event_type,
+            "src_path": event.src_path,
         }
-        # print(json.dumps(data))
-        _LOGGER.warning("WATCHDOG_CALLED with {} {}".format(
-            data["event"], data["file"]))
-        self._event_data.update(data)
+        self.fire_event(data)
 
     def on_modified(self, event):
+        self.process(event)
+
+    def on_moved(self, event):
         self.process(event)
 
     def on_created(self, event):
