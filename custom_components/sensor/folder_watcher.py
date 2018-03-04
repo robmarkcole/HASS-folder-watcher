eac093aca8cdfb695876d76ecc40c3d60567721f
robmarkcole <robmarkcole@gmail.com>
2018-03-04 15:36:19 +0000

    v0.1
    successfully logs watchdog activity

--- custom_components/sensor/folder_watcher.py
+++ custom_components/sensor/folder_watcher.py
@@ -1,11 +1,16 @@
 """
 Sensor for monitoring activity on a folder.
 """
-import datetime
-from datetime import timedelta
+#import datetime
+#from datetime import timedelta
 import logging
 import os
 import voluptuous as vol
+#import sys
+import time
+import json
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
@@ -13,38 +18,20 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_FOLDER_PATH = 'folder'
-
-SCAN_INTERVAL = timedelta(seconds=1)
+CONF_PATH = 'folder'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_FOLDER_PATH): cv.isdir,
+    vol.Required(CONF_PATH): cv.isdir,
 })
-
-
-def get_timestamp(dir_entry):
-    """Return the timestamp of file modification."""
-    mtime = dir_entry.stat().st_mtime
-    return datetime.datetime.fromtimestamp(mtime).isoformat()
-
-
-def get_files(path):
-    """Return the dict of files and timestamps."""
-    files = {}
-    with os.scandir(path) as it:
-        for entry in it:
-            if not entry.name.startswith('.') and entry.is_file():
-                files[entry.name] = get_timestamp(entry)
-    return files
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the folder watcher."""
-    folder_path = config.get(CONF_FOLDER_PATH)
-    if not hass.config.is_allowed_path(folder_path):
-        _LOGGER.error("folder %s is not valid or allowed", folder_path)
+    path = config.get(CONF_PATH)
+    if not hass.config.is_allowed_path(path):
+        _LOGGER.error("folder %s is not valid or allowed", path)
     else:
-        folder_watcher = Watcher(folder_path, hass)
+        folder_watcher = Watcher(path)
         add_devices([folder_watcher], True)
 
 
@@ -53,49 +40,17 @@ class Watcher(Entity):
 
     ICON = 'mdi:folder'
 
-    def __init__(self, folder_path, hass):
-        self._folder_path = os.path.join(folder_path, '')  # Ass trailing /
-        self._hass = hass
-        self._updated = False
-        self._files = get_files(self._folder_path)
-        self._name = os.path.split(
-            os.path.split(self._folder_path)[0])[1] + "_watcher"
+    def __init__(self, path):
+        self._path = os.path.join(path, '')  # Ass trailing /
         self._state = None
-
-    def update(self):
-        """Update the watcher."""
-        current_files = []
-        previous_files = list(self._files.keys())
-
-        with os.scandir(self._folder_path) as folder:
-            for entry in folder:
-                if not entry.name.startswith('.') and entry.is_file():
-                    mtime = get_timestamp(entry)
-                    fname = entry.name
-                    current_files.append(fname)  # Keep record of current files
-
-                    # If file not in files list, add the entry
-                    if fname not in self._files:
-                        self._hass.bus.fire('file_added', {'file': fname})
-                        self._state = fname + " added"
-                        self._files[fname] = mtime
-
-                    # If exists and modified, update timestamp
-                    elif fname in self._files and self._files[fname] != mtime:
-                        self._hass.bus.fire('file_modified', {'file': fname})
-                        self._state = fname + " modified"
-                        self._files[fname] = mtime
-
-            # Check if any files deleted
-            for fname in list(set(previous_files) - set(current_files)):
-                self._hass.bus.fire('file_deleted', {'file': fname})
-                self._state = fname + " deleted"
-                self._files.pop(fname, None)  # Delete the entry
+        self._observer = Observer()
+        self._observer.schedule(MyHandler(), self._path, recursive=True)
+        self._observer.start()
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return "folder_watcher"
 
     @property
     def state(self):
@@ -111,6 +66,33 @@ class Watcher(Entity):
     def device_state_attributes(self):
         """Return other details about the sensor state."""
         attr = {
-            'path': self._folder_path
+            'path': self._path
             }
         return attr
+
+
+class MyHandler(PatternMatchingEventHandler):
+    patterns = ["*.txt", "*.py", "*.md", "*.jpg", "*.png"]
+
+    def process(self, event):
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        data = {
+            "time": time.strftime("%Y-%m-%d %H:%M"),
+            "event": event.event_type,
+            "full_path": event.src_path,
+            "file": os.path.split(event.src_path)[-1]
+        }
+        # print(json.dumps(data))
+        _LOGGER.warning("WATCHDOG_CALLED")
+        _LOGGER.warning(json.dumps(data))
+
+    def on_created(self, event):
+        self.process(event)
