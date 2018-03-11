2e5f49c8e621d667d0442c9a876fe09d031d7804
robmarkcole <robmarkcole@gmail.com>
2018-03-11 14:00:49 +0000

    Update config, rename process to on_any_event
    Stay consistent with watchdog function naming

--- custom_components/folder_watcher.py
+++ custom_components/folder_watcher.py
@@ -23,15 +23,13 @@ EVENT_TYPE = "event_type"
 FILE = 'file'
 FOLDER = 'folder'
 
-WATCHER_CONFIG_SCHEMA = vol.Schema([{
-    vol.Required(CONF_FOLDER): cv.isdir,
-    vol.Optional(CONF_PATTERNS, default=[DEFAULT_PATTERN]): vol.All(
-        cv.ensure_list, [cv.string]),
-}])
-
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.All(cv.ensure_list, WATCHER_CONFIG_SCHEMA)
-    }, extra=vol.ALLOW_EXTRA)
+    DOMAIN: vol.All(cv.ensure_list, [vol.Schema({
+        vol.Required(CONF_FOLDER): cv.isdir,
+        vol.Optional(CONF_PATTERNS, default=[DEFAULT_PATTERN]):
+            vol.All(cv.ensure_list, [cv.string]),
+    })])
+}, extra=vol.ALLOW_EXTRA)
 
 
 def setup(hass, config):
@@ -60,7 +58,7 @@ def create_event_handler(patterns, hass):
             super().__init__(patterns)
             self.hass = hass
 
-        def process(self, event):
+        def on_any_event(self, event):
             """On Watcher event, fire HA event."""
             if not event.is_directory:
                 folder_path, file_name = os.path.split(event.src_path)
@@ -73,19 +71,19 @@ def create_event_handler(patterns, hass):
 
         def on_modified(self, event):
             """File modified."""
-            self.process(event)
+            self.on_any_event(event)
 
         def on_moved(self, event):
             """File moved."""
-            self.process(event)
+            self.on_any_event(event)
 
         def on_created(self, event):
             """File created."""
-            self.process(event)
+            self.on_any_event(event)
 
         def on_deleted(self, event):
             """File deleted."""
-            self.process(event)
+            self.on_any_event(event)
 
     return EventHandler(patterns, hass)
 
